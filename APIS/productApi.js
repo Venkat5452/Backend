d0539661cf7916db5236391a2fc2793bfea31afd
Rajesh T <Rajesh@Rajeshs-MacBook-Pro.local>
2022-03-29 19:31:53 +0530

    product API is implemented

--- APIS/productApi.js
+++ APIS/productApi.js
@@ -1,69 +1,93 @@
 //create a special route to handle product reqs
-const { workspaceSchemaPath } = require('@angular/cli/utilities/config');
-const exp=require('express');
-const productApp=exp.Router();
+
+const exp = require("express");
+const productApp = exp.Router();
+
+const expressAsyncHandler=require('express-async-handler');
+const res = require("express/lib/response");
 
 //to extract body of request object
 productApp.use(exp.json());
 
 //get all products
-productApp.get('/getproducts',(request,response)=>{
-  response.send({message:"all products"})
-})
+productApp.get("/getproducts", expressAsyncHandler(async(request,response)=>{
+
+  //get productCollectionObject
+  let productCollectionObject = request.app.get("productCollectionObject");
+  //read all products
+  let products=await productCollectionObject.find().toArray()
+  //send response
+  response.send({message:"All products",payload:products})
+}));
+
+
+
+
 
 //get product by id
-productApp.get("/getproduct/:id",(request,response)=>{
-
-  response.send({message:`product with id ${request.params.id}`})
-})
-
-
-//to create product
-// productApp.post('/create-product',(request,response)=>{
- 
-//   //get productCollectionObject
-//   let productCollectionObject=request.app.get("productCollectionObject")
-//   //get product obj from req
-//   let productObj=request.body;
-//   //insert productObj
-//   productCollectionObject.insertOne(productObj,(err,result)=>{
-//     if(err){
-//       console.log("err in creating product ",err)
-//     }
-//     else{
-//       response.send({message:'Product created successfylly'})
-//     }
-//   })
-
-// })
-
-//creating proudct using promise
-// productApp.post('/create-product',(request,response)=>{
- 
-//   //get productCollectionObject
-//   let productCollectionObject=request.app.get("productCollectionObject")
-//   //get product obj from req
-//   let productObj=request.body;
-//   //insert productObj
-//   productCollectionObject.insertOne(productObj)
-//   .then((result)=>response.send({message:'Product created successfylly'}))
-//   .catch(err=> console.log("err in creating product ",err))
-
-// })
-
-//create product with async n await
-productApp.post('/create-product',async(request,response)=>{
- 
+productApp.get("/getproduct/:id", expressAsyncHandler(async(request,response)=>{
   //get productCollectionObject
-  let productCollectionObject=request.app.get("productCollectionObject")
+  let productCollectionObject = request.app.get("productCollectionObject");
+  //get productId from url param
+  let pid=(+request.params.id);
+  //get product by id
+  let product=await productCollectionObject.findOne({productId:pid});
+
+  
+  //if product not existed with given id
+  if(product==null){
+    response.send({message:'product not existed'})
+  }
+  //if product existed
+  else{
+  response.send({message:'product existed',payload:product})
+  }
+
+}))
+
+
+
+
+
+
+//create product
+productApp.post("/create-product", expressAsyncHandler(async (request, response) => {
+  
+  
+  //get productCollectionObject
+  let productCollectionObject = request.app.get("productCollectionObject");
   //get product obj from req
-  let productObj=request.body;
+  let productObj = request.body;
   //insert productObj
-  let result=await productCollectionObject.insertOne(productObj)
+  let result = await productCollectionObject.insertOne(productObj);
   //send response
-  response.send({message:'Product created successfylly'})
-})
+  response.send({ message: "Product created successfully" });
+  
+ 
+}));
+
+
+//update product
+productApp.put('/update-product',expressAsyncHandler(async(request,response)=>{
+  //get productCollectionObject
+  let productCollectionObject = request.app.get("productCollectionObject");
+  //get modified product obj
+  let modifiedProduct=request.body;
+  //update
+  await productCollectionObject.updateOne({productId:modifiedProduct.productId},{$set:{...modifiedProduct}})
+  //send response
+  response.send({message:"Product modified"})
+}))
+
+
+//delete product by id
+productApp.delete("/remove-product/:id",expressAsyncHandler(async(request,response)=>{
+
+  //get productCollectionObject
+  let productCollectionObject = request.app.get("productCollectionObject");
+  //write logic to delete product by its id
+}))
 
 
 //export productApp
-module.exports=productApp;
+module.exports = productApp;
